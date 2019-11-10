a52d98057c0bfd9dfdd304729bc3b229debfb062
Luo-Leng <55864809+Luo-Leng@users.noreply.github.com>
2019-11-09 19:49:45 -0800

    Delete CTestTestfile.cmake

--- problem_3/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/e/git/class6/homework/problem_3/test
-# Build directory: /mnt/e/git/class6/homework/problem_3/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit "/mnt/e/git/class6/homework/problem_3/unit_test")
